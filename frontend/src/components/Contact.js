efd084ae18ff12c5b75d24ee810a80fd8f77a40e
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-04-26 15:19:48 +0200

    Added elements for text messaging

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,6 +1,26 @@
 import { info } from "../pages/MainPage";
+import { useState, useEffect, useRef } from "react";
+import { fetchJson } from "../scripts/Fetch";
 
 export function Contact(props) {
+  const [messages, setMessages] = useState([]);
+  const ref = useRef(false);
+
+  async function fetchMessages() {
+    const response = await fetchJson(
+      `/message?contactName=${
+        props.profile.contacts[props.selectedContact].username
+      }`,
+      "GET"
+    );
+
+    if (response.status < 400) {
+      setMessages(await response.json());
+    } else {
+      console.log(await response.text());
+    }
+  }
+
   async function callClick() {
     info.conn = window.peer.connect(
       props.profile.contacts[props.selectedContact].username
@@ -24,12 +44,34 @@ export function Contact(props) {
     props.setCall(true);
   }
 
+  useEffect(() => {
+    if (!ref.current) {
+      ref.current = true;
+      fetchMessages();
+    }
+  });
+
   return (
     <div className="contact-page">
       {props.selectedContact !== undefined && props.profile && (
         <>
-          <p>{props.profile.contacts[props.selectedContact].username}</p>
-          <button onClick={callClick}>Call</button>
+          <div className="contact-top">
+            <p>{props.profile.contacts[props.selectedContact].username}</p>
+            <div className="contact-btns">
+              <button onClick={callClick}>📞</button>
+            </div>
+          </div>
+          <div className="contact-chat-container">
+            <div className="chat-message-container">
+              {messages.map((message) => (
+                <p>{message.content}</p>
+              ))}
+            </div>
+            <div className="chat-input-container">
+              <button>➕</button>
+              <input placeholder="Type here..." />
+            </div>
+          </div>
         </>
       )}
     </div>
