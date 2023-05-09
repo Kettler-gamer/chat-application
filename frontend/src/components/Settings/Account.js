bc1e6e44368fdaf3d04b1434cf5f522345ad7ed7
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-05-09 11:53:36 +0200

    added logout button

--- frontend/src/components/Settings/Account.js
+++ frontend/src/components/Settings/Account.js
@@ -4,12 +4,20 @@ import { ChangePassword } from "./AccountRoutes/ChangePassword";
 export function Account(props) {
   const navigate = useNavigate();
 
+  function onLogout() {
+    sessionStorage.removeItem("jwtToken");
+    navigate("/");
+  }
+
   return (
     <>
       <button
         className="option-btn"
         onClick={() => navigate("/main/settings/account/changepassword")}>
         Change password
+      </button>
+      <button className="option-btn" onClick={onLogout}>
+        Logout
       </button>
       <Routes>
         <Route path="changepassword" element={<ChangePassword />} />
