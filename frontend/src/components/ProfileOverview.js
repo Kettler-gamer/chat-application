74fe59eef92f0418469eca2a42c0ce5d17efc091
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-05-18 14:11:35 +0200

    added remove contact button

--- frontend/src/components/ProfileOverview.js
+++ frontend/src/components/ProfileOverview.js
@@ -1,15 +1,59 @@
+import { useState } from "react";
+
 export function ProfileOverview({ profile }) {
-  console.log(profile);
+  const [cat, setCat] = useState("contacts");
+  const [currentContact, setCurrentContact] = useState("");
+
+  let list;
+  switch (cat) {
+    case "contacts":
+      list = profile?.contacts || [];
+      break;
+    case "requests":
+      list = profile?.requests || [];
+      break;
+    case "blocked":
+      list = profile?.blocked || [];
+      break;
+    default:
+      list = [];
+  }
+
+  const buttonNames = ["Contacts", "Requests", "Blocked"];
+
+  function onCatClick(newCat) {
+    setCat(newCat);
+    setCurrentContact("");
+  }
+
+  function onContactOptionClick(contact) {
+    if (contact === currentContact) {
+      return setCurrentContact("");
+    }
+
+    setCurrentContact(contact);
+  }
+
+  function onRemoveContactClick(contact) {
+    // to be implemented
+    console.log(contact);
+  }
+
   return (
     <div className="contact-page">
       <div className="contact-top">
-        <button className="profile-overview-btn">Contacts</button>
-        <button className="profile-overview-btn">Requests</button>
-        <button className="profile-overview-btn">Blocked</button>
+        {buttonNames.map((btnName) => (
+          <button
+            key={btnName}
+            className="profile-overview-btn"
+            onClick={() => onCatClick(btnName.toLowerCase())}>
+            {btnName}
+          </button>
+        ))}
       </div>
       <div className="contact-list-container">
         <ul className="contact-list">
-          {profile?.contacts.map((contact, index) => (
+          {list.map((contact, index) => (
             <li key={`contact-${index}`} className="list-item">
               <div
                 className="profile-picture"
@@ -19,6 +63,26 @@ export function ProfileOverview({ profile }) {
                   })`,
                 }}></div>
               <p>{contact.username}</p>
+              <div className="contact-btns">
+                <button onClick={() => onContactOptionClick(contact.username)}>
+                  <svg>
+                    <rect y={10} x={9} height={2} rx={2} width={22} />
+                    <rect y={18} x={9} height={2} rx={2} width={22} />
+                    <rect y={26} x={9} height={2} rx={2} width={22} />
+                  </svg>
+                  <ul
+                    className="option-list"
+                    style={
+                      contact.username === currentContact
+                        ? {}
+                        : { display: "none" }
+                    }>
+                    <li onClick={() => onRemoveContactClick(contact)}>
+                      <p>Remove</p>
+                    </li>
+                  </ul>
+                </button>
+              </div>
             </li>
           ))}
         </ul>
