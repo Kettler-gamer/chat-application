9cc4d479e97919d08a357abc622ae22e8e7bf397
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-05-19 14:11:16 +0200

    added profile pictures are now croppable

--- frontend/src/components/Settings/Profile.js
+++ frontend/src/components/Settings/Profile.js
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { fetchJson } from "../../scripts/Fetch";
+
+const image = new Image();
+let trackMouse = false;
+let x = 0,
+  y = 0;
 
 export function Profile(props) {
   const [picture, setPicture] = useState(undefined);
+  const canvasRef = useRef(null);
+
   async function updateProfilePic() {
+    const imageUrl = canvasRef.current.toDataURL();
     const response = await fetchJson("/profilePicture", "PUT", {
-      picture: picture,
+      picture: imageUrl,
     });
 
     if (response.status < 400) {
       props.setProfile((oldValue) => {
         const newValue = oldValue;
-        newValue.profilePicture = picture;
+        newValue.profilePicture = imageUrl;
         return newValue;
       });
     }
@@ -31,27 +39,75 @@ export function Profile(props) {
 
       fileReader.onloadend = (e) => {
         setPicture(e.target.result);
+        setTimeout(() => {
+          renderCanvas(e.target.result);
+        }, 250);
       };
 
       fileReader.readAsDataURL(file);
     }
   }
 
+  function renderCanvas(imageUrl) {
+    image.src = imageUrl;
+    image.onload = () => {
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.drawImage(image, 0, 0);
+    };
+  }
+
+  function onMouseDown() {
+    trackMouse = true;
+  }
+
+  function onMouseUp() {
+    trackMouse = false;
+  }
+
+  function onMouseMove(e) {
+    if (!trackMouse) return;
+    x += -e.movementX;
+    y += -e.movementY;
+
+    x = Math.max(0, x);
+    y = Math.max(0, y);
+
+    x = Math.min(image.width, x);
+    y = Math.min(image.height, y);
+
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, 512, 512);
+    ctx.drawImage(image, -x, -y);
+  }
+
   return (
     <>
       <div className="picture-switch">
-        <div
-          className="profile-pic"
-          style={{
-            backgroundImage: `url(${
-              picture ||
-              props.profile?.profilePicture ||
-              "/images/profile-pic.webp"
-            })`,
-          }}></div>
-        <label htmlFor="picture-upload">
-          <p>Replace</p>
-        </label>
+        {picture ? (
+          <>
+            <canvas
+              width={512}
+              height={512}
+              ref={canvasRef}
+              onMouseDown={onMouseDown}
+              onMouseUp={onMouseUp}
+              onMouseMove={onMouseMove}
+            />
+          </>
+        ) : (
+          <div
+            className="profile-pic"
+            style={{
+              backgroundImage: `url(${
+                props.profile?.profilePicture || "/images/profile-pic.webp"
+              })`,
+            }}></div>
+        )}
+        {!picture && (
+          <label htmlFor="picture-upload">
+            <p>Replace</p>
+          </label>
+        )}
         <input
           id="picture-upload"
           className="profile-pic-input"
